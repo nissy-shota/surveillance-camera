44695e916db578fddf1e5b0af01c38e4dc4dc4ad
nissy-shota <shota_4410@icloud.com>
2021-12-19 09:52:26 +0900

    face_detector_deep

---
+++ demo/face_detection/face_detector_deep.py
@@ -0,0 +1,55 @@
+import cv2
+from facenet_pytorch import MTCNN
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+
+class FaceDetector:
+    '''
+    face detection class.
+    Attributes:
+        img_path (str): image path
+        img (PIL): PIL Object which has range (-1, 1)
+        img_cropped(tensor): cropped img by mtcnn
+        detected_img(numpy array): detecte face image which has range (-1, 1)
+
+    Examples:
+        >>> detector = FaceDetector(image_path)
+        >>> detector.detecte_face()
+        >>> detector.save()
+        >>> detector.show()
+    '''
+
+    def __init__(self, img_path):
+        self.img_path = img_path
+
+    def detecte_face(self):
+        '''
+        detect face
+        Args: None
+        return: None
+        '''
+
+        mtcnn = MTCNN()
+        self.img = Image.open(self.img_path)
+        self.img_cropped = mtcnn(self.img)
+
+    def save(self, save_path):
+        '''
+        save face image
+        Args: save_path
+        return: None
+        '''
+        detected_img = self.img_cropped.to('cpu').detach().numpy().copy()
+        self.detected_img = np.transpose(detected_img, (1, 2, 0))
+        cv2.imwrite(save_path, self.detected_img)
+
+    def show(self):
+        '''
+        show face image
+        Args: save_path
+        return: None
+        '''
+        plt.imshow(self.detected_img)
+        plt.show()
