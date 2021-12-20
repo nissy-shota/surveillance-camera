17c0dc6f7ee8cbb425d8e52e58cb5ecfb69ae20c
nissy-shota <shota_4410@icloud.com>
2021-12-21 02:02:24 +0900

    fix save path

--- demo/face_detection/main.py
+++ demo/face_detection/main.py
@@ -1,8 +1,8 @@
 import os
 from face_detector_deep import FaceDetector
 
-IMG_DIR = '/home/shota/Projects/surveillance-camera/debug/face_detection/img'
-RESULTS_DIR = '/home/shota/Projects/surveillance-camera/debug/face_detection/results'
+IMG_DIR = '/home/shota/Projects/surveillance-camera/demo/face_detection/img'
+RESULTS_DIR = '/home/shota/Projects/surveillance-camera/demo/face_detection/results'
 
 def main():
 
