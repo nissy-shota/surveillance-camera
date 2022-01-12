e51f4a96b478167aa3c28d4cb4e87edd128dccd4
Hiroki Narita <k17099kk@aitech.ac.jp>
2022-01-12 14:57:06 +0900

    flask_app/app.pyで起動するように変更

--- flask_app/app.py
+++ flask_app/app.py
@@ -9,7 +9,7 @@ def top():
 
 @app.route("/capture")
 def capture():
-    cp = subprocess.Popen(["python", "../main.py"])
+    cp = subprocess.Popen(["python", "main.py"])
     return render_template("capture.html")
 
 if __name__ == '__main__':
